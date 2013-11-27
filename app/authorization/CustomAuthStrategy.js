12bdcb073951049bfd4c15f534ae3b45594089cc
amir-arad <greenshade@gmail.com>
2013-11-28 01:51:00 +0200

    refactoring authorization folder

---
+++ app/authorization/CustomAuthStrategy.js
@@ -0,0 +1,39 @@
+/**
+ * Generic local authorization strategy
+ * User: amira
+ * Date: 11/27/13
+ * Time: 11:03 PM
+ */
+
+var express = require('express');
+var util = require('util');
+var LocalStrategy = require('passport-local').Strategy;
+
+module.exports = function (app, config, passport, strategyName, getUserFromReq, isAuthorized, authorizeFallback){
+
+    util.inherits(CustomStrategy, LocalStrategy);
+
+    function CustomStrategy() {
+        CustomStrategy.super_.call(this, {
+            usernameField: 'password',
+            passwordField: 'password',
+            passReqToCallback: true
+        }, function (req, password, password, done) {
+            return done(null, getUserFromReq(req, password));
+        });
+        this.name = strategyName;
+    }
+
+    passport.use(new CustomStrategy());
+
+    this.authorize = function(req, res, next){
+        if (isAuthorized(req)){
+            return next();
+        } else {
+            return authorizeFallback(req, res, next);
+        }
+    };
+
+    this.authenticate = [express.multipart(), passport.authenticate(strategyName)];
+
+};
--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,13 @@
 
 module.exports = function(app, config, passport) {
-    var admin = new (require('../app/authorization/admin'))(app, config, passport);
-    var adminAuth = admin.authorize(function(req, res){           // fallback to auth error
-        res.setHeader('WWW-Authenticate', 'Basic realm="Artifacts"');
-        res.send(401, { "msg" : "error.no.credentials" });    // Unauthorized
-    });
+    // init the 3 authorization strategies  :admin, storyTeller, player
+    var auth = new (require('../app/authorization/Strategies'))(app, config, passport,
+        function(req, res){           // fallback to auth error
+            res.setHeader('WWW-Authenticate', 'Basic realm="Artifacts"');
+            res.send(401, { "msg" : "error.no.credentials" });    // Unauthorized
+        });
+
+
     app.post('/logout', function(req, res, next){
         req.logout();
         res.send(204);         // OK, No Content
@@ -12,65 +15,60 @@ module.exports = function(app, config, passport) {
     var silentOk = function (req, res) {
         res.send(204);         // OK, No Content
     };
-    app.post('/login', admin.authenticate, silentOk);
+    app.post('/login', auth.admin.authenticate, silentOk);
 
     var games = new (require('../app/controllers/Games'))(app, config);
     app.param('gameId', games.game);
-    app.get('/games', adminAuth, games.list);
-    app.post('/games', adminAuth, games.create);
+    app.get('/games', auth.admin.authorize, games.list);
+    app.post('/games', auth.admin.authorize, games.create);
 
+    app.post('/games/:gameId/login', auth.storyteller.authenticate, silentOk);
 
-    var storyteller = new (require('../app/authorization/storyteller'))(app, config, passport);
-    var storytellerAuth = storyteller.authorize(adminAuth);
-    app.post('/games/:gameId/login', storyteller.authenticate, silentOk);
-
-    app.get('/games/:gameId', storytellerAuth, games.show);
-    app.put('/games/:gameId', storytellerAuth, games.update);
-    app.del('/games/:gameId', storytellerAuth, games.destroy);
+    app.get('/games/:gameId', auth.storyteller.authorize, games.show);
+    app.put('/games/:gameId', auth.storyteller.authorize, games.update);
+    app.del('/games/:gameId', auth.storyteller.authorize, games.destroy);
 
     var players = new (require('../app/controllers/Players'))(app, config);
     app.param('playerId', players.player);
-    app.get('/games/:gameId/players', storytellerAuth, players.list);
-    app.post('/games/:gameId/players', storytellerAuth, players.create);
-    app.get('/games/:gameId/players/:playerId', storytellerAuth, players.show);
-    app.put('/games/:gameId/players/:playerId', storytellerAuth, players.update);
-    app.del('/games/:gameId/players/:playerId', storytellerAuth, players.destroy);
+    app.get('/games/:gameId/players', auth.storyteller.authorize, players.list);
+    app.post('/games/:gameId/players', auth.storyteller.authorize, players.create);
+    app.get('/games/:gameId/players/:playerId', auth.storyteller.authorize, players.show);
+    app.put('/games/:gameId/players/:playerId', auth.storyteller.authorize, players.update);
+    app.del('/games/:gameId/players/:playerId', auth.storyteller.authorize, players.destroy);
 
 
-    var player = new (require('../app/authorization/player'))(app, config, passport);
-    var playerAuth = player.authorize(storytellerAuth);
-    app.post('/games/:gameId/players/:playerId/login', player.authenticate, silentOk);
+    app.post('/games/:gameId/players/:playerId/login', auth.player.authenticate, silentOk);
 
     var artifacts =  new (require('../app/controllers/Artifacts'))(app, config);
     app.param('artifactId', artifacts.artifact);
-    app.get('/games/:gameId/artifacts', storytellerAuth, artifacts.listByGame);
-    app.get('/games/:gameId/players/:playerId/artifacts', playerAuth, artifacts.listByPlayer);
+    app.get('/games/:gameId/artifacts', auth.storyteller.authorize, artifacts.listByGame);
+    app.get('/games/:gameId/players/:playerId/artifacts', auth.player.authorize, artifacts.listByPlayer);
 
     // TODO get artifacts on ground by location + report location
 
-    app.post('/games/:gameId/artifacts', storytellerAuth, artifacts.create);
-    app.get('/games/:gameId/artifacts/:artifactId', playerAuth, artifacts.show);
-    app.put('/games/:gameId/artifacts/:artifactId', storytellerAuth, artifacts.update);
-    app.del('/games/:gameId/artifacts/:artifactId', storytellerAuth, artifacts.destroy);
+    app.post('/games/:gameId/artifacts', auth.storyteller.authorize, artifacts.create);
+    app.get('/games/:gameId/artifacts/:artifactId', auth.player.authorize, artifacts.show);
+    app.put('/games/:gameId/artifacts/:artifactId', auth.storyteller.authorize, artifacts.update);
+    app.del('/games/:gameId/artifacts/:artifactId', auth.storyteller.authorize, artifacts.destroy);
     // todo artifact pick, drop w/ playerAuth
 
 
-   // var multipart = new (require('../app/controllers/Multipart'))(app, config);
+    // var multipart = new (require('../app/controllers/Multipart'))(app, config);
     var assets = new (require('../app/controllers/Assets'))(app, config);
     app.param('assetId', assets.asset);
-    app.get('/games/:gameId/assets', storytellerAuth, assets.list);
-    app.get('/games/:gameId/assets/:assetId', storytellerAuth, assets.show);
-    app.post('/games/:gameId/assets', storytellerAuth, assets.create);
-    app.put('/games/:gameId/assets/:assetId', storytellerAuth, assets.update);
-    app.del('/games/:gameId/assets/:assetId', storytellerAuth, assets.destroy);
+    app.get('/games/:gameId/assets', auth.storyteller.authorize, assets.list);
+    app.get('/games/:gameId/assets/:assetId', auth.storyteller.authorize, assets.show);
+    app.post('/games/:gameId/assets', auth.storyteller.authorize, assets.create);
+    app.put('/games/:gameId/assets/:assetId', auth.storyteller.authorize, assets.update);
+    app.del('/games/:gameId/assets/:assetId', auth.storyteller.authorize, assets.destroy);
 
-    app.get('/games/:gameId/artifacts/:artifactId/assets', playerAuth, assets.list);
-    app.post('/games/:gameId/artifacts/:artifactId/assets', storytellerAuth, assets.create);
-    app.put('/games/:gameId/artifacts/:artifactId/assets/:assetId', storytellerAuth, assets.update);
-    app.del('/games/:gameId/artifacts/:artifactId/assets/:assetId', storytellerAuth, assets.update);
+    app.get('/games/:gameId/artifacts/:artifactId/assets', auth.player.authorize, assets.list);
+    app.post('/games/:gameId/artifacts/:artifactId/assets', auth.storyteller.authorize, assets.create);
+    app.put('/games/:gameId/artifacts/:artifactId/assets/:assetId', auth.storyteller.authorize, assets.update);
+    app.del('/games/:gameId/artifacts/:artifactId/assets/:assetId', auth.storyteller.authorize, assets.update);
 
     // specific hack for nicely display of artifact to the user
     app.param('assetFileName', assets.assetAsFile);
-    app.get('/games/:gameId/artifacts/:artifactId/:assetFileName', playerAuth, assets.show);
+    app.get('/games/:gameId/artifacts/:artifactId/:assetFileName', auth.player.authorize, assets.show);
 
 }
