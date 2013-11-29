13e97ccaea6a7e1a65b9efd200929f51c44ad15d
amir-arad <greenshade@gmail.com>
2013-11-29 15:32:17 +0200

    made startup gridFs optional

--- config/mongo.js
+++ config/mongo.js
@@ -9,25 +9,32 @@
 var mongodb = require('mongodb');
 
 module.exports = function(app, config, next) {
-
-// Bootstrap db connection
-// from http://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
+    // Bootstrap db connection
     mongodb.MongoClient.connect(config.db, function(err, db) {
         if(err) return next(err, null);
-        // test write-read-delete operations on gridFS
-        var grid = new mongodb.Grid(db, config.gridfs.bucket);
-        var buffer = new Buffer("Test buffer");
-        grid.put(buffer, {metadata:{category:'text'}, content_type: 'text', fileName:'startup.test'}, function(err, fileInfo) {
-            if(err) return next(err, null);
-            app.logger.debug("Finished writing file to Mongo");
-            grid.get(fileInfo._id, function(err, data) {
-                if(err) return next(err, null);
-                app.logger.debug("Finished reading file from Mongo");
-                grid.delete(fileInfo._id, function(err, result) {
-                    if(err) return next(err, null);
-                    app.logger.debug("Finished deleting file from Mongo");
-                    return next(null, db);
-                });
+        if (config.gridfs.initTest){
+            testFiles(db, config, next, app);
+        } else {
+            return next(null, db);
+        }
+    });
+}
+
+function testFiles(db, config, next, app) {
+    app.logger.info("testing write-read-delete operations on gridFS");
+    var grid = new mongodb.Grid(db, config.gridfs.bucket);
+    var buffer = new Buffer("Test buffer");
+    grid.put(buffer, {metadata: {category: 'text'}, content_type: 'text', fileName: 'startup.test'}, function (err, fileInfo) {
+        if (err) return next(err, null);
+        app.logger.debug("Finished writing file to Mongo");
+        grid.get(fileInfo._id, function (err, data) {
+            if (err) return next(err, null);
+            app.logger.debug("Finished reading file from Mongo");
+            grid.delete(fileInfo._id, function (err, result) {
+                if (err) return next(err, null);
+                app.logger.debug("Finished deleting file from Mongo");
+                app.logger.info("successful write-read-delete operations on gridFS");
+                return next(null, db);
             });
         });
     });
