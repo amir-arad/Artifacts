5a31d15b55de7e341eba4b88fa86adfaa4b4adb7
amir-arad <greenshade@gmail.com>
2013-12-15 01:53:18 +0200

    fix small login nav thing

--- public/player/js/controllers.js
+++ public/player/js/controllers.js
@@ -4,35 +4,19 @@ player.controllers = {};
 angular.module('player.controllers', []).
     controller(player.controllers);
 
-// from http://www.yearofmoo.com/2012/10/more-angularjs-magic-to-supercharge-your-webapp.html#apply-digest-and-phase
-// be sure to inject $scope and $location
-var changeLocation = function($scope, $location, url, forceReload) {
-    $scope = $scope || angular.element(document).scope();
-    if(forceReload || $scope.$$phase) {
-        window.location = url;
-    }
-    else {
-        //only use this if you want to replace the history stack
-        //$location.path(url).replace();
-        //this this if you want to change the URL and add it to the history stack
-        $location.path(url);
-        $scope.$apply();
-    }
-};
-
 /**
  * The login controller.
  */
-player.controllers.loginController =  function ($scope, $log, $location, authService) {
+player.controllers.loginController =  function ($scope, $log, $location, $navigate, authService) {
     $scope.login = function() {
         $log.debug('login sent', $scope.game, $scope.player, $scope.password);
         authService.login($scope.game, $scope.player, $scope.password).then(function(){
-            changeLocation($scope, $location, "#/inventory");
+            $navigate.go('/inventory');
         });
     };
     authService.isLoggedIn().then(function(isLoggedIn){
         if ($location.path() == '/login' && isLoggedIn){
-            changeLocation($scope, $location, "#/inventory");
+            $navigate.go('/inventory');
         }
     });
 };
@@ -55,6 +39,11 @@ player.controllers.headerController =  function ($scope, $location, $navigate, a
         "title": "Look Around",
         "link": "nearby"
     }];
+    authService.isLoggedIn().then(function(isLoggedIn){
+        if ($location.path() != '/login' && !isLoggedIn){
+            $navigate.go('/login');
+        }
+    });
 };
 
 player.controllers.alertsController =  function($scope, alertService) {
