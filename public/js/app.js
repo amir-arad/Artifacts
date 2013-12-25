1377f9f067395616bc1b6dad43a873eca38a3f29
amir-arad <greenshade@gmail.com>
2013-12-26 00:13:11 +0200

    clean ups, made an index

--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-window.app = angular.module('artifacts',
-    ['ngCookies', 'restangular', 'ui.bootstrap', 'ui.route', 'artifacts.system', 'mean.articles']);
-
-angular.module('artifacts.system', []);
-angular.module('mean.articles', []);
